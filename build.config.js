3df9e0b4871377bdaeec9fec24859950ce607c0e
Viktor Nagy <viktor.nagy@gmail.com>
2014-05-17 09:11:25 +0200

    better timing

--- build.config.js
+++ build.config.js
@@ -69,7 +69,8 @@ module.exports = {
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
-      'vendor/angular-resource/angular-resource.min.js'
+      'vendor/angular-resource/angular-resource.min.js',
+      'vendor/momentjs/min/moment-with-langs.min.js'
     ],
     css: [
       'vendor/bootstrap/dist/js/bootstrap.min.css',
